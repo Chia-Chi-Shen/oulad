3ee620d2992a125bfcb40cd327daf4ffcd54a36e
Chia-Chi-Shen <r12227108@ntu.edu.tw>
2025-06-02 00:05:16 +0800

    some changes

--- modeling_v2.R
+++ modeling_v2.R
@@ -1,7 +1,8 @@
 library(dplyr)
 library(lme4)
+library(sjPlot)
 
-data <- data %>%
+data_normalized <- data_normalized %>%
   mutate(
     score_bin = ifelse(score == "group3", 1, 0),
     module_presentation = as.factor(module_presentation),
@@ -13,18 +14,27 @@ data <- data %>%
   )
 
 model <- glmer(
-  score_bin ~ gender + region + highest_education + age_band + 
-    late_ratio + 
-    homepage_mean_clicks + homepage_mean_click_interval + #homepage_std_click_interval + 
-    input_std_click_interval + input_mean_clicks +  
-    output_mean_click_interval + output_mean_clicks + output_click_variance + #output_std_click_interval + 
-    early_score + late_score + mid_score +
+  score_bin ~ gender + region + highest_education + age_band + imd_band + 
+    homepage_mean_clicks + homepage_mean_click_interval + homepage_click_variance + homepage_std_click_interval + 
+    input_mean_click_interval + input_mean_clicks + input_click_variance + input_std_click_interval + 
+    output_mean_click_interval + output_mean_clicks + output_click_variance + output_std_click_interval + 
+    early_score + mid_score + late_score + late_ratio + 
     (1 | module_presentation),  # 隨機截距
-  data = data,
-  family = binomial(link = "logit")
+  data = data_normalized,
+  family = binomial(link = "logit"),
+  control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 2e5))
 )
 summary(model)
 
+base_model <- glmer(
+  score_bin ~ region + highest_education + age_band + imd_band + gender + 
+    (1 | module_presentation),  # 隨機截距
+  data = data_normalized,
+  family = binomial(link = "logit")
+)
+summary(base_model)
+
+tab_model(base_model, model, )
 library(performance)
 
 r2_res <- r2(model)   # 計算 R²
